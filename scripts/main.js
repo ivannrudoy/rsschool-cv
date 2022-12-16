1737ffa0753237a660ef5dbc818eb9a0f9d6424a
ivan <ivannrudoy@gmail.com>
2022-12-16 18:38:54 +0300

    feat: add toggle active link

--- scripts/main.js
+++ scripts/main.js
@@ -10,12 +10,13 @@ const setLinkActive = (link) => link.classList.add(TEXT_ACTIVE);
  */
 const setLinkidle = (link) => link.classList.remove(TEXT_ACTIVE);
 
-
 /**
  * @param {Document} doc
  */
 const handleNavbarBehaviour = (doc) => {
-  /**
+  let activeId = 0;
+
+    /**
    * @type {HTMLElement}
    */
   const navbar = doc.getElementById("navbar");
@@ -25,6 +26,25 @@ const handleNavbarBehaviour = (doc) => {
    */
   let prevLink = navbar.getElementsByTagName("A")[0];
   setLinkActive(prevLink);
+
+  const clickListener = (event) => {
+    /**
+     * @type {HTMLElement}
+     */
+    const targetItem = event.target;
+
+    if (targetItem.tagName === "UL" || targetItem.tagName === "NAV") return;
+
+    const prevId = activeId;
+    activeId = targetItem.dataset.id;
+    if (activeId !== prevId) {
+      setLinkidle(prevLink);
+      setLinkActive(targetItem);
+      prevLink = targetItem;
+    }
+  };
+
+  navbar.addEventListener("click", clickListener);
 };
 
 const LoadListener = () => {
